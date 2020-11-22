50d511555598a51ffeae04699cb217131b3d232a
hosam mohamed <hosamelmalt@gmail.com>
2020-11-22 07:53:47 -0800

    Initial Commit

---
+++ Speces-E/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Speces-E
+//
+//  Created by Hossam on 11/22/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Speces-E/Speces_EApp.swift
@@ -0,0 +1,17 @@
+//
+//  Speces_EApp.swift
+//  Speces-E
+//
+//  Created by Hossam on 11/22/20.
+//
+
+import SwiftUI
+
+@main
+struct Speces_EApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
